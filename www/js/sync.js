3077a9495cf84f6bc8b5ec0191c09c1e8fabcccc
Alejandro St <animatk@gmail.com>
2015-03-19 16:21:39 -0500

    webworker prueba 3

--- www/js/sync.js
+++ www/js/sync.js
@@ -1,11 +1,16 @@
 /*! web worker */
 self.addEventListener('message', function(e) {
-	sincronizar({ 
-		url : e.data, 
-		,res: function(resp){
-			self.postMessage(resp);
-		}
-	});
+	//sincronizar({ 
+	//	url : e.data, 
+	//	,res: function(resp){
+	//		self.postMessage(resp);
+	//	}
+	//});
+	
+	setTimeout(function(){
+		self.postMessage('El Worker se esta ejecutando :: recibio '+ e.data);
+	}, 2000);
+	
 }, false);
 /*! web worker */
 
