0d750223277dc9d61bfb40a045e3a7579e711712
Alejandro St <animatk@gmail.com>
2015-06-04 20:11:32 -0500

    animacion al boton de inicio prueba dos

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -86,7 +86,6 @@ $(function(){
 		}
 	}, false);
 });
-
 function botonCancel(){
 	if(btnCancel){
 		setTimeout(function(){
@@ -101,7 +100,6 @@ function botonCancel(){
 		}, 50);
 	}
 }
-
 function swipeHorz(box, func, durante){
 	var startx = 0, dist = 0;
 	//
